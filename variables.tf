b6ae0528c7517ddbc377f26378e89e8b81649bb5
sowjiarha <128574497+sowjiarha@users.noreply.github.com>
2023-05-01 13:42:24 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "domainName" {
+  default = "dev.sowjitest.com"
+  type = string
+}
+variable "bucketName" {
+  default = "dev.sowjitest.com"
+  type = string
+}
+variable "acm_arn" {
+  default = "arn:aws:acm:us-east-1:058179552518:certificate/9151e530-9da6-46ff-be98-8faa9564736d"
+  type = string
+}
